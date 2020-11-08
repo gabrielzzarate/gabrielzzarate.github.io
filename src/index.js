824a7ebe22915e022899185b79a1d1542a63e3be
Gabriel Zarate <gabrielzzarate@gmail.com>
2020-11-08 17:04:47 -0500

    both pages finished

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
+import 'normalize.css';
 import './sass/styles.scss';
+
+$(document).ready(function() {
+
+
+  $('.hamburger').click(function() {
+    const hamburger = $('.hamburger');
+    const header = $('.header');
+    const body = $('body');
+
+    if (hamburger.hasClass('active')) {
+      hamburger.removeClass('active');
+      body.removeClass('fix-window');
+      return header.removeClass('active');
+    } else {
+      body.addClass('fix-window');
+      hamburger.addClass('active');
+      return header.addClass('active');
+    }
+  });
+
+  $('.slider').slick({
+    infinite: false,
+    dots: true,
+    autoplay: true,
+    speed: 300,
+    slidesToShow: 1,
+    fade: true,
+    arrows: false,
+    cssEase: 'linear'
+  });
+});
